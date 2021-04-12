81108723d53f4ad04f13f5c6d4e55a57b2fb85f9
PRINCE GUTIERREZ <81812004+PDGHACK-10@users.noreply.github.com>
2021-04-12 22:55:45 +0800

    Create hacking-tools.sh

---
+++ hacking-tools.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+clear
+blue='\033[31;1m'
+red='\033[32;1m'
+green='\033[33;1m'
+white='\033[34;1m'
+
+dependencies() {
+
+command -v git > /dev/null 2>&1 || { echo >&2 "I require git but it's not installed. Install it. Aborting."; exit 1; }
+  
+  
+
+
+}
+
+
+
+tools(){
+
+echo -e $red CODED BY : PRINCE GUTIERREZ
+echo -e $red YOUTUBE CHANNEL : PDGHACK-12
+echo -e $red v2.1
+
+echo -e $red "[1] > FACEBOOK-BRUTE-FORCE"
+echo -e $red "[2] > MLBB-TOOL {MOBILE-LEGENDS HACKINGTOOL}"
+echo -e $red "[3] > CamHack"
+echo -e $red "[4] > Sn1per"
+echo -e $red "[5] > TOOL-X"
+echo -e $red "[6] > Hack-Tools"
+echo -e $red "[7] > hacktronian"
+echo -e $red "[8] > onex"
+echo -e $red "[9] > HACKINGTOOL"
+echo -e $red "[10] > HACKER-TOOLS"
+echo -e $red "[11] > NGROK"
+echo -e $red "[12] > MALICIOUS"
+read -p "PLEASE SELECT OPTION " hug
+
+if [[ $hug == 1 || $hug == 01 ]]; then
+
+elif [[ $hug == 2 || $hug == 02 ]]; then
+
+elif [[ $hug == 3 || $hug == 03 ]]; then
+
+elif [[ $hug == 4 || $hug == 04 ]]; then
+
+elif [[ $hug == 5 || $hug == 05 ]]; then
+
+elif [[ $hug == 6 || $hug == 06 ]]; then
+
+elif [[ $hug == 7 || $hug == 07 ]]; then
+
+elif [[ $hug == 8 || $hug == 08 ]]; then
+
+elif [[ $hug == 9 || $hug == 09 ]]; then
+
+elif [[ $hug == 10 || $hug == 10 ]]; then
+
+elif [[ $hug == 11 || $hug == 11 ]]; then
+
+elif [[ $hug == 12 || $hug == 12 ]]; then
+
+
+
+
+
+
+
+
