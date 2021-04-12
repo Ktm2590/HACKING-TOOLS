aa58567979dc0fdf3bfb583e577fba6b37cfbf04
PRINCE GUTIERREZ <81812004+PDGHACK-10@users.noreply.github.com>
2021-04-13 00:23:08 +0800

    Update hacking-tools.sh

--- hacking-tools.sh
+++ hacking-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-clear
+##coded by prince gutierrez
 blue='\033[31;1m'
 red='\033[32;1m'
 green='\033[33;1m'
@@ -23,47 +23,46 @@ echo -e $red YOUTUBE CHANNEL : PDGHACK-12
 echo -e $red v2.1
 
 echo -e $red "[1] > FACEBOOK-BRUTE-FORCE"
-echo -e $red "[2] > MLBB-TOOL {MOBILE-LEGENDS HACKINGTOOL}"
-echo -e $red "[3] > CamHack"
-echo -e $red "[4] > Sn1per"
-echo -e $red "[5] > TOOL-X"
-echo -e $red "[6] > Hack-Tools"
-echo -e $red "[7] > hacktronian"
-echo -e $red "[8] > onex"
-echo -e $red "[9] > HACKINGTOOL"
-echo -e $red "[10] > HACKER-TOOLS"
-echo -e $red "[11] > NGROK"
-echo -e $red "[12] > MALICIOUS"
+echo -e $red "[2] > HACKER-TOOLS"
+echo -e $red "[3] > HACKINGTOOLS"
+echo -e $red "[4] > HACKTRONIAN"
+echo -e $red "[5] > MALICIOUS"
+echo -e $red "[6] > MLBB-HACK"
+echo -e $red "[7] > NGROK"
+echo -e $red "[8] > ONEX"
+echo -e $red "[9] > TOOL-X"
+
 read -p "PLEASE SELECT OPTION " hug
 
 if [[ $hug == 1 || $hug == 01 ]]; then
+cd TOOLS && bash FB-BRUTE
 
 elif [[ $hug == 2 || $hug == 02 ]]; then
+cd TOOLS && bash HACKER-TOOLS
 
 elif [[ $hug == 3 || $hug == 03 ]]; then
+cd TOOLS && bash HACKINGTOOLS
 
 elif [[ $hug == 4 || $hug == 04 ]]; then
+cd TOOLS && bash HACKTRONIAN
 
 elif [[ $hug == 5 || $hug == 05 ]]; then
+cd TOOLS && bash MALICIOUS
 
 elif [[ $hug == 6 || $hug == 06 ]]; then
+cd TOOLS && bash MLBB-TOOL
 
 elif [[ $hug == 7 || $hug == 07 ]]; then
+cd TOOLS && bash NGROK
 
 elif [[ $hug == 8 || $hug == 08 ]]; then
+cd TOOLS && bash ONEX
 
 elif [[ $hug == 9 || $hug == 09 ]]; then
+cd TOOLS && bash TOOL-X
+fi
 
-elif [[ $hug == 10 || $hug == 10 ]]; then
+}
 
-elif [[ $hug == 11 || $hug == 11 ]]; then
-
-elif [[ $hug == 12 || $hug == 12 ]]; then
-
-
-
-
-
-
-
-
+dependencies
+tools
